6a0d046bfa96c49ac11a4120ac2561ff37e108b4
ashutoshpith <amxdp18@gmail.com>
2019-11-01 00:49:17 +0530

    Category is created with full crud operations

--- Spice/Areas/Admin/Controllers/CategoryController.cs
+++ Spice/Areas/Admin/Controllers/CategoryController.cs
@@ -44,7 +44,24 @@ namespace Spice.Areas.Admin.Controllers
             }
             return View(category);
         }
+        
+        // GET: Earths/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
+            var category = await _db.Category
+                .FirstOrDefaultAsync(s => s.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -75,6 +92,45 @@ namespace Spice.Areas.Admin.Controllers
             }
 
             return View(category);
+        }
+
+
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+
+            var category = await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmation(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var category =await _db.Category.FindAsync(id);
+            if (category == null)
+            {
+                return View();
+            }
+            _db.Category.Remove(category);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
         }
 
     }
